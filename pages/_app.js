c51ae8abd23a2cba8f18ae133ac7b16ce01a9a08
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-07-23 03:33:46 +0100

    fix all import alias setup issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "antd/dist/antd.css";
+import "@/styles/vars.css";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
